cdb40f3d82310581c9b44f21afa59e20944f73cf
Léonard Binet <leonardbinet@gmail.com>
2019-10-18 01:34:13 +0200

    agg node field type test

--- tests/nodes/test_agg_nodes.py
+++ tests/nodes/test_agg_nodes.py
@@ -1,4 +1,4 @@
-from pandagg.nodes.agg_nodes import AggNode, BucketAggNode
+from pandagg.nodes.agg_nodes import AggNode
 from unittest import TestCase
 
 
@@ -9,11 +9,20 @@ class AggNodesTestCase(TestCase):
         class CustomAgg(AggNode):
             AGG_TYPE = 'custom_type'
             VALUE_ATTRS = ['bucket_value_path']
+            # would mean this agg can be applied only on boolean fields
+            WHITELISTED_MAPPING_TYPES = ['boolean']
+            BLACKLISTED_MAPPING_TYPES = None
+            # means it ES response produces a single bucket
+            SINGLE_BUCKET = True
 
             # depends on ElasticSearch aggregation handling, since this is a fake Aggregation this get_filter method
             # doesn't really make sense, just wrote one so that all abstract methods are implemented
             def get_filter(self, key):
                 return {'exists': {'field': key}}
+
+            # example for unique bucket agg
+            def extract_buckets(self, response_value):
+                yield (None, response_value)
 
         node = CustomAgg(agg_name='custom_agg_name', agg_body={'custom_body': {'stuff': 2}})
         self.assertEqual(
@@ -50,6 +59,9 @@ class AggNodesTestCase(TestCase):
             'bucket_value_path': 43
         }
         self.assertEqual(node.extract_bucket_value(hypothetic_es_response_bucket), 43)
+
+        self.assertEqual(node.valid_on_field_type('string'), False)
+        self.assertEqual(node.valid_on_field_type('boolean'), True)
 
     def test_bucket_node(self):
         # BucketAggNodes can have children aggregations
